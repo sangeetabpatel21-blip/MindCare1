65ef6cb392f79e8b6e8b084f9a94beda6623fc95
sangeetabpatel21-blip <sangeetabpatel21@gmail.com>
2025-11-30 16:52:17 +0530

    Delete App.tsx

--- App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div style={{ backgroundColor: 'black', minHeight: '100vh', color: 'white' }}>
-      <h1>Hello from MindCare</h1>
-    </div>
-  );
-}
-
-export default App;
